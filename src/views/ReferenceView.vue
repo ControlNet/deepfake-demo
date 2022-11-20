d8e7e4d833730e13202b5289a6e9237866002aa2
ControlNet <smczx@hotmail.com>
2022-11-21 06:05:31 +1100

    update references

--- src/views/ReferenceView.vue
+++ src/views/ReferenceView.vue
@@ -3,13 +3,16 @@
     <h2>References</h2>
     <ul>
       <li>
-        <p>Face Attribute Manipulation using GANs. (2018). Retrieved from https://towardsdatascience.com/face-attribute-manipulation-using-gans-9fae92e9f1c3</p>
+        <p>Radhakrishnan, P. (2018). Face attribute manipulation using Gans. Towards Data Science. Retrieved from https://towardsdatascience.com/face-attribute-manipulation-using-gans-9fae92e9f1c3</p>
       </li>
       <li>
-        <p>Google's AI Clones Your Voice After Listening for 5 Seconds!. (2022). Retrieved from https://www.youtube.com/watch?v=0sR1rU3gLzQ</p>
+        <p>Two Minute Papers. (2019). Google's ai clones your voice after listening for 5 Seconds! 🤐. YouTube. Retrieved from https://www.youtube.com/watch?v=0sR1rU3gLzQ</p>
       </li>
       <li>
-        <p>The rise of the deepfake and the threat to democracy. (2022). Retrieved from https://www.theguardian.com/technology/ng-interactive/2019/jun/22/the-rise-of-the-deepfake-and-the-threat-to-democracy</p>
+        <p>Parkin, S. (2019). The rise of the Deepfake and the threat to democracy. The Guardian. Retrieved from https://www.theguardian.com/technology/ng-interactive/2019/jun/22/the-rise-of-the-deepfake-and-the-threat-to-democracy</p>
+      </li>
+      <li>
+        <p>Mintoadil. (2020). Create your own deep fake. Medium. Retrieved from https://mintoadil.medium.com/create-your-own-deep-fake-7d6e66a23559</p>
       </li>
       <li>
         <p>Jia, Y., Zhang, Y., Weiss, R. J., Wang, Q., Shen, J., Ren, F., Chen, Z., Nguyen, P., Pang, R., Moreno, I. L., & Wu, Y. (2018). Transfer learning from speaker verification to multispeaker text-to-speech synthesis. Proceedings of the 32nd International Conference on Neural Information Processing Systems, 4485–4495.</p>
